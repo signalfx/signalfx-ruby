bdc6211d9c9eeb8ad35dfb6793783d77c281a549
Ben Keith <bkeith@signalfx.com>
2020-01-10 18:46:25 +0000

    Bump version to 3.0.0 for release

--- lib/signalfx/version.rb
+++ lib/signalfx/version.rb
@@ -1,8 +1,8 @@
-# Copyright (C) 2015-2016 SignalFx, Inc. All rights reserved.
+# Copyright (C) 2015-2020 SignalFx, Inc. All rights reserved.
 
 module SignalFx
   module Version
-    VERSION = '2.1.0'
+    VERSION = '3.0.0'
     NAME = 'signalfx-ruby-client'
   end
 end
